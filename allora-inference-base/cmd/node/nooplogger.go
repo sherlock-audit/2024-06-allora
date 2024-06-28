736dd580f57dbf49dd31bc1332db92d21fde6bd7
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-06-28 10:35:57 +0200

    Delete scope directories

--- allora-inference-base/cmd/node/nooplogger.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type pebbleNoopLogger struct{}
-
-func (p *pebbleNoopLogger) Infof(_ string, _ ...any) {}
-
-func (p *pebbleNoopLogger) Fatalf(_ string, _ ...any) {}
-
-func (p *pebbleNoopLogger) Errorf(_ string, _ ...any) {}
--- allora-inference-base/launch-node.sh
+++
@@ -1,9 +0,0 @@
-allora-node \
---peer-db /tmp/avs/head-peer-db \
---function-db /tmp/avs/head-fdb \
---log-level debug \
---port 9527 \
---role head \
---workspace /tmp/debug/head \
---private-key ./testkeys/head/priv.bin \
---rest-api :8081
